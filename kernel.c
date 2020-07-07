dcc43a88003f700a75d10c747a228e761edfe35d
Jooarye <jooris2@schule.bremen.de>
2020-07-07 09:34:43 +0200

    Screen Management
    Added print function with color support
    Added clear function

--- kernel.c
+++ kernel.c
@@ -1,30 +1,47 @@
-void kmain(void)
-{
-	const char *str = "my first kernel";
-	char *vidptr = (char*)0xb8000; 	//video mem begins here.
+void print(char *str, int color) {
+	char *vidpos = (char*)0x7c00;
+	char *vidptr = (char*)0xb8000;
+
 	unsigned int i = 0;
-	unsigned int j = 0;
-
-	/* this loops clears the screen
-	* there are 25 lines each of 80 columns; each element takes 2 bytes */
-	while(j < 80 * 25 * 2) {
-		/* blank character */
-		vidptr[j] = ' ';
-		/* attribute-byte - light grey on black screen */
-		vidptr[j+1] = 0x07; 		
-		j = j + 2;
+	while (str[i] != '\0') {
+		if (str[i] == '$') {
+			color = str[i+1] - 'a';
+			i += 2;
+		} else {
+			vidptr[*vidpos * 2] = str[i];
+			vidptr[*vidpos * 2 + 1] = color;
+			*vidpos = *vidpos + 1;
+			i++;
+		}
 	}
 
-	j = 0;
+	*vidpos += 80 - (*vidpos % 80);
 
-	/* this loop writes the string to video memory */
-	while(str[j] != '\0') {
-		/* the character's ascii */
-		vidptr[i] = str[j];
-		/* attribute-byte: give character black bg and light grey fg */
-		vidptr[i+1] = 0x07;
-		++j;
-		i = i + 2;
+}
+
+void clear() {
+	char *vidpos = (char*)0x7c00;
+	char *vidptr = (char*)0xb8000;
+
+	*vidpos = 0;
+
+	unsigned int i = 0;
+
+	while (i < 80 * 25 * 2) {
+		vidptr[i] = ' ';
+		vidptr[i + 1] = 0x07;
+
+		i += 2;
 	}
+}
+
+void kmain(void) {
+	char *vidpos = (char*)0x7c00;
+	*vidpos = 0;
+
+	clear();
+
+	print("[$cInfo$h] Initializing screen buffer", 0x07);
+
 	return;
 }
