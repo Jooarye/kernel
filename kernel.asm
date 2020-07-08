e185770df7c5ba822cdf9a4abf4b7f0fb419351f
Jooarye <jooris2@schule.bremen.de>
2020-07-08 10:20:10 +0200

    Changing color system

--- kernel.asm
+++ kernel.asm
@@ -18,6 +18,23 @@ start:
   call kmain
   hlt		 	;halt the CPU
 
+read_port:
+	mov edx, [esp + 4]
+	in al, dx	
+	ret
+
+write_port:
+	mov   edx, [esp + 4]    
+	mov   al, [esp + 4 + 4]  
+	out   dx, al  
+	ret
+
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti
+	ret
+
 section .bss
 resb 8192		;8KB for stack
 stack_space:
--- kernel.c
+++ kernel.c
@@ -1,27 +1,58 @@
+typedef unsigned char uint8;
+
+
+enum VGAColor {
+	BLACK = 0,
+	BLUE = 1,
+	GREEN = 2,
+	CYAN = 3,
+	RED = 4,
+	MAGENTA = 5,
+	BROWN = 6,
+	LIGHT_GREY = 7,
+	DARK_GREY = 8,
+	LIGHT_BLUE = 9,
+	LIGHT_GREEN = 10,
+	LIGHT_CYAN = 11,
+	LIGHT_RED = 12,
+	LIGHT_MAGENTA = 13,
+	LIGHT_BROWN = 14,
+	WHITE = 15
+};
+
+uint8 composeColor(enum VGAColor fg, enum VGAColor bg) {
+	return fg | bg << 4;
+}
+
 void kprint(char *str, int color) {
-	char *vidpos = (char*)0x7c00;
-	char *vidptr = (char*)0xb8000;
+	uint8 *vidpos = (uint8*)0x7c00;
+	uint8 *vidptr = (uint8*)0xb8000;
 
 	unsigned int i = 0;
-	while (str[i] != '\0') {
-		if (str[i] == '$') {
-			color = str[i+1] - 'a';
-			i += 2;
-		} else {
-			vidptr[*vidpos * 2] = str[i];
-			vidptr[*vidpos * 2 + 1] = color;
-			*vidpos = *vidpos + 1;
-			i++;
-		}
-	}
 
-	*vidpos += 80 - (*vidpos % 80);
+	while (str[i] != '\0') {
+		vidptr[*vidpos * 2] = str[i];
+		vidptr[*vidpos * 2 + 1] = color;
+		*vidpos = *vidpos + 1;
+		i++;
+	}
 
 }
 
-void kclear() {
-	char *vidpos = (char*)0x7c00;
-	char *vidptr = (char*)0xb8000;
+void knewline() {
+	uint8 *vidpos = (uint8*)0x7c00;
+
+	*vidpos += 80 - (*vidpos % 80);
+}
+
+void kprintln(char *str, int color) {
+	kprint(str, color);
+	knewline();
+}
+
+void kclear(uint8 color) {
+	uint8 *vidpos = (uint8*)0x7c00;
+	uint8 *vidptr = (uint8*)0xb8000;
 
 	*vidpos = 0;
 
@@ -29,20 +60,29 @@ void kclear() {
 
 	while (i < 80 * 25 * 2) {
 		vidptr[i] = ' ';
-		vidptr[i + 1] = 0x07;
+		vidptr[i + 1] = color;
 
 		i += 2;
 	}
 }
 
 void kmain(void) {
-	char *vidpos = (char*)0x7c00;
+	uint8 *vidpos = (uint8*)0x7c00;
 	*vidpos = 0;
 
-	kclear();
+	uint8 color = composeColor(LIGHT_GREY, BLACK);
 
-	kprint("[$cInfo$h] Initialized screen buffer!", 0x07);
-	kprint("[$eError$h] Failed to load kernel modules!", 0x07);
+	kclear(color);
+
+	kprint("[", color);
+	kprint(" OK ", composeColor(BLACK, LIGHT_GREEN));
+	kprint("] Initialized screen buffer!", color);
+	knewline();
+
+	kprint("[", color);
+	kprint("FAIL", composeColor(WHITE, RED));
+	kprint("] Failed to load kernel modules!", color);
+	knewline();
 
 	return;
 }
